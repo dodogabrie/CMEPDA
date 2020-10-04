9816f88066a7b91a5a9246110cd485c25a64607b
dodogabrie <dodogabrie97@live.it>
2020-10-04 11:20:03 +0200

    divise features da main

--- a1/my_charcount.py
+++ a1/my_charcount.py
@@ -2,62 +2,29 @@ import os                                       # Manipulate path of my PC
 import argparse                                 # Make --help description
 import logging                                  # Make Debug and Warning 
 import time                                     # Misure time of process
-import math
-import matplotlib.pyplot as plt                # for the istogram
-import numpy as np
+import my_features
 logging.basicConfig(level=logging.INFO)         # Definisce il livello di log
 
 
 _description = 'Measure the releative frequencies of letters in a text file'
 
-
-def istogram(yn,freq_dict):
-    if yn == 'y':                               # Evito di eseguire se non serve
-        letters = []                            # Inizializzo vuoti, riempio nel
-        vec = []                                #   loop scorrendo il dizionario
-
-        # Riempie i miei vettori per il plot scorrendo il dizionario.
-        for ch, freq in freq_dict.items():
-            letters.append(ch)
-            vec = np.append(vec,freq*100)
-
-        # stampa l'istogramma su volontà dell'utente.
-        plt.bar(letters, height=vec)
-        plt.grid(axis='y', alpha=0.75)
-        plt.xlabel('Value')
-        plt.ylabel('Frequency')
-        plt.title('Letters Frequencies')
-        maxfreq = vec.max()
-        plt.ylim(ymax=maxfreq+1)
-        plt.show()
-    else:
-        return
-
-
-def print_res(freq_dict):
-    # Stampa le percentuali, un istogramma ascii e scrive dentro a vec i valori percentuali
-    for ch, freq in freq_dict.items():
-        if freq*100. < 10:
-            #   lettera: perc                   esempio: 5% = #####, la funzione ceil arrotonda.
-            print('{}: {:.3f}% '.format(ch, freq*100.), '#'*math.ceil( freq*100. - 0.5 ))
-        else:
-            print('{}: {:.3f}%'.format(ch, freq*100.), '#'*math.ceil( freq*100. - 0.5 ))
-
-def process(file_path,inst):
-    start = time.time()                         # Start measuring time
+def process(file_path,inst,sk,stats):
     """Main processing method.
     """
+    start = time.time()                         # Start measuring time
     # Basic sanity check: make sure that the file_argument points to an
     # existing text file.
-    assert file_path.endswith('.txt')
-    assert os.path.isfile(file_path)
+    assert file_path.endswith('.txt'), "Not a txt file"
+    assert os.path.isfile(file_path), "Wrong path"
+    test_inst = (inst == "y" or inst == "n")
+    test_sk = (sk == "y" or sk == "n")
+    test_stats = (stats == "y" or stats == "n")
+    assert  test_inst and test_sk and test_stats, "Can insert only y/n"
 
     # Open the input file (note that we are taking advantage of context
     # management, using a with statement).
-    logging.info('Opening input file "%s"', file_path)
-    with open(file_path) as input_file:             # Apre e chiude file  
-        data = input_file.read()
-    logging.info('Done. %d character(s) found.', len(data))
+    data = my_features.extract(file_path,sk)
+
     # Prepare a dictionary to hold the letter frequencies, and initialize
     # all the counts to zero.
     letters = 'abcdefghijklmnopqrstuvwxyz'
@@ -78,15 +45,17 @@ def process(file_path,inst):
 
     # We're done---print the glorious output. (And here it is appropriate to
     # use print() instead of logging.)
-    print_res(freq_dict)                        # Print Result with ascii istogram
+    my_features.print_freq(freq_dict)                        # Print Result with ascii istogram
     end = time.time()                           # Stop measuring time
-    istogram(inst,freq_dict)                      # Print Instrogram from matplotlib
+    my_features.istogram(inst,freq_dict)                      # Print Instrogram from matplotlib
     logging.info('Elapsed Time: %f sec', end-start)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=_description)
     parser.add_argument('infile', help='path to the input file')
-    parser.add_argument('--inst', type=str , default='n', help='add matplot istogram [y/n]')
+    parser.add_argument('--inst', type=str , default='n', help='Add matplot istogram [y/n]')
+    parser.add_argument('--skip', type=str , default='n', help='Skip preamble and license [y/n]')
+    parser.add_argument('--stats', type=str , default='n', help='Print basic book stats [y/n]')
     args = parser.parse_args()
-    process(args.infile,args.inst)
+    process(args.infile,args.inst,args.skip,args.stats)
